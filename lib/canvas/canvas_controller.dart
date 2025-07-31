e855018fc053de06afe4e1976042b745e5a823d2
chutne9 <kimhuyss@icloud.com>
2025-07-31 08:11:40 +0700

    Add method for animate to item

--- lib/canvas/canvas_controller.dart
+++ lib/canvas/canvas_controller.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
 
 class CanvasController extends ChangeNotifier {
-  static const double _minScale = 0.01;
-  static const double _maxScale = 10.0;
+  static const double minScale = 0.1;
+  static const double maxScale = 4.0;
 
   Matrix4 _transform = Matrix4.identity();
+  AnimationController? _animationController;
+  Animation<Matrix4>? _animation;
+  final TickerProvider? vsync;
+
+  CanvasController({this.vsync});
 
   Matrix4 get transform => _transform;
 
   set transform(Matrix4 newTransform) {
+    if (_animationController?.isAnimating ?? false) {
+      _animationController!.stop();
+    }
+    _setTransform(newTransform);
+  }
+
+  void _setTransform(Matrix4 newTransform) {
     if (_transform == newTransform) return;
     _transform = newTransform;
     notifyListeners();
   }
 
-  /// Pans the canvas by a given delta.
-  ///
-  /// The [delta] is the amount to move the canvas in screen coordinates.
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
   void pan(Offset delta) {
     final translationMatrix = Matrix4.identity()..translate(delta.dx, delta.dy);
     transform = translationMatrix * _transform;
   }
 
-  /// Zooms the canvas by a given scale delta, centered on a focal point.
-  ///
-  /// The [scaleDelta] is the multiplicative factor for the zoom.
-  /// Values > 1.0 zoom in, values < 1.0 zoom out.
-  ///
-  /// The [focalPoint] is the point on the screen (e.g., cursor position)
-  /// where the zoom should be centered.
   void zoom(double scaleDelta, Offset focalPoint) {
     final double currentScale = _transform.getMaxScaleOnAxis();
     double newScale = currentScale * scaleDelta;
 
-    if (newScale < _minScale) {
-      scaleDelta = _minScale / currentScale;
-    } else if (newScale > _maxScale) {
-      scaleDelta = _maxScale / currentScale;
+    if (newScale < minScale) {
+      scaleDelta = minScale / currentScale;
+    } else if (newScale > maxScale) {
+      scaleDelta = maxScale / currentScale;
     }
 
     if (scaleDelta == 1.0) return;
@@ -47,5 +55,33 @@ class CanvasController extends ChangeNotifier {
       ..translate(-focalPoint.dx, -focalPoint.dy);
 
     transform = zoomMatrix * _transform;
+  }
+
+  void animateTo(
+    Matrix4 target, {
+    Duration duration = const Duration(milliseconds: 250),
+    Curve curve = Curves.easeInOut,
+  }) {
+    if (vsync case final vsync?) {
+      _animationController?.stop();
+      _animationController ??= AnimationController(vsync: vsync)
+        ..addListener(_onAnimate);
+      _animationController!.duration = duration;
+
+      _animation = Matrix4Tween(
+        begin: _transform,
+        end: target,
+      ).animate(CurvedAnimation(parent: _animationController!, curve: curve));
+
+      _animationController!
+        ..reset()
+        ..forward();
+    }
+  }
+
+  void _onAnimate() {
+    if (_animation case final animation?) {
+      _setTransform(animation.value);
+    }
   }
 }
