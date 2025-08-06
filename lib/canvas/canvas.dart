60905655bb8c72bce4d00dfe465bed9f971e7c33
chutne9 <kimhuyss@icloud.com>
2025-08-06 20:56:01 +0700

    Update pan/zoom interaction

--- lib/canvas/canvas.dart
+++ lib/canvas/canvas.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:infinite_canvas_viewer/canvas/canvas_controller.dart';
 import 'package:infinite_canvas_viewer/canvas/canvas_grid_painter.dart';
 import 'package:infinite_canvas_viewer/canvas/canvas_layout.dart';
@@ -10,7 +12,7 @@ class InfiniteCanvasViewer extends StatefulWidget {
     required this.controller,
     required this.children,
     this.gridType = GridType.none,
-    this.backgroundColor = const Color(0xFFFDFDFD),
+    this.backgroundColor = const Color(0xFF1a1b26),
     this.canZoom = true,
     this.canPan = true,
     this.foregroundPainter,
@@ -28,58 +30,230 @@ class InfiniteCanvasViewer extends StatefulWidget {
   State<InfiniteCanvasViewer> createState() => _InfiniteCanvasViewerState();
 }
 
-class _InfiniteCanvasViewerState extends State<InfiniteCanvasViewer> {
-  double _previousScale = 1.0;
+class _InfiniteCanvasViewerState extends State<InfiniteCanvasViewer>
+    with SingleTickerProviderStateMixin {
+  final FocusNode _focusNode = FocusNode();
+
+  bool _isSpacePressed = false;
+  bool _isLeftMouseDragging = false;
+  bool _isMiddleMousePanning = false;
+
+  Matrix4 _gestureStartMat = Matrix4.identity();
+  Offset _gestureStartPosition = Offset.zero;
+  Offset _gestureStartLocalPosition = Offset.zero;
+
+  final double _dragZoomSensitivity = 0.002;
+  final double _scrollZoomSensitivity = 0.001;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.setVsync(this);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        FocusScope.of(context).requestFocus(_focusNode);
+      }
+    });
+  }
 
   @override
   void dispose() {
-    widget.controller.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
-  void _handlePointerSignal(PointerSignalEvent event) {
-    if (event is PointerScrollEvent && widget.canZoom) {
-      const double zoomSensitivity = 200.0;
-      final double scaleDelta = 1.0 - event.scrollDelta.dy / zoomSensitivity;
-      final Offset focalPoint = event.localPosition;
-      widget.controller.zoom(scaleDelta, focalPoint);
+  KeyEventResult _handleKeyEvent(KeyEvent event) {
+    final bool spacePressedCurrently = HardwareKeyboard.instance
+        .isLogicalKeyPressed(LogicalKeyboardKey.space);
+
+    if (_isSpacePressed != spacePressedCurrently) {
+      widget.controller.stopAnimation();
+      setState(() {
+        _isSpacePressed = spacePressedCurrently;
+      });
+      return KeyEventResult.handled;
+    }
+
+    return _isSpacePressed ? KeyEventResult.handled : KeyEventResult.ignored;
+  }
+
+  MouseCursor get _cursor {
+    if (_isMiddleMousePanning) {
+      return SystemMouseCursors.grabbing;
+    }
+    if (_isLeftMouseDragging) {
+      return _isSpacePressed
+          ? SystemMouseCursors.grabbing
+          : SystemMouseCursors.zoomIn;
+    }
+    if (_isSpacePressed) {
+      return SystemMouseCursors.grab;
+    }
+    return MouseCursor.defer;
+  }
+
+  void _handleMouseScroll(PointerScrollEvent event) {
+    if (!widget.canZoom) return;
+
+    widget.controller.stopAnimation();
+    final double scaleDelta =
+        1.0 - event.scrollDelta.dy * _scrollZoomSensitivity;
+    widget.controller.zoom(scaleDelta, event.localPosition);
+  }
+
+  void _onPointerDown(PointerDownEvent event) {
+    if (!_focusNode.hasFocus) {
+      _focusNode.requestFocus();
+    }
+    if (event.buttons == kMiddleMouseButton && widget.canPan) {
+      if (_isLeftMouseDragging) return;
+      widget.controller.stopAnimation();
+      setState(() {
+        _isMiddleMousePanning = true;
+        _gestureStartMat = widget.controller.transform.clone();
+        _gestureStartPosition = event.position;
+      });
     }
   }
 
-  void _handleScaleStart(ScaleStartDetails details) {
-    _previousScale = 1.0;
-  }
-
-  void _handleScaleUpdate(ScaleUpdateDetails details) {
-    if (widget.canPan) {
-      widget.controller.pan(details.focalPointDelta);
-    }
-
-    if (widget.canZoom) {
-      final double scaleDelta = details.scale / _previousScale;
-      if (scaleDelta != 1.0) {
-        widget.controller.zoom(scaleDelta, details.localFocalPoint);
+  void _onPointerMove(PointerMoveEvent event) {
+    if (_isMiddleMousePanning) {
+      if (event.buttons & kMiddleMouseButton == 0) {
+        setState(() => _isMiddleMousePanning = false);
+        return;
       }
-      _previousScale = details.scale;
+      final Offset delta = event.position - _gestureStartPosition;
+      final panMatrix = Matrix4.identity()..translate(delta.dx, delta.dy);
+      widget.controller.transform = panMatrix * _gestureStartMat;
+    }
+  }
+
+  void _onPointerUp(PointerUpEvent event) {
+    if (_isMiddleMousePanning && (event.buttons & kMiddleMouseButton) == 0) {
+      setState(() => _isMiddleMousePanning = false);
+    }
+  }
+
+  void _onPointerCancel(PointerCancelEvent event) {
+    if (_isMiddleMousePanning) {
+      setState(() => _isMiddleMousePanning = false);
+    }
+    if (_isLeftMouseDragging) {
+      setState(() => _isLeftMouseDragging = false);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: widget.backgroundColor,
+    return Focus(
+      focusNode: _focusNode,
+      onKeyEvent: (node, event) => _handleKeyEvent(event),
+      autofocus: true,
       child: Listener(
-        onPointerSignal: _handlePointerSignal,
-        child: GestureDetector(
-          onScaleStart: _handleScaleStart,
-          onScaleUpdate: _handleScaleUpdate,
-          behavior: HitTestBehavior.opaque,
-          child: ClipRect(
-            child: CanvasLayoutWidget(
-              controller: widget.controller,
-              gridType: widget.gridType,
-              foregroundPainter: widget.foregroundPainter,
-              children: widget.children,
+        onPointerDown: _onPointerDown,
+        onPointerMove: _onPointerMove,
+        onPointerUp: _onPointerUp,
+        onPointerCancel: _onPointerCancel,
+        onPointerSignal: (pointerSignal) {
+          if (pointerSignal is PointerScrollEvent) {
+            _handleMouseScroll(pointerSignal);
+          }
+        },
+        child: MouseRegion(
+          cursor: _cursor,
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onPanStart: (details) {
+              if (_isMiddleMousePanning) return;
+              if (!_isSpacePressed &&
+                  details.kind != PointerDeviceKind.trackpad) {
+                return;
+              }
+              if (!_focusNode.hasFocus) {
+                FocusScope.of(context).requestFocus(_focusNode);
+              }
+              widget.controller.stopAnimation();
+              setState(() {
+                _isLeftMouseDragging = true;
+                _gestureStartMat = widget.controller.transform.clone();
+                _gestureStartPosition = details.globalPosition;
+                _gestureStartLocalPosition = details.localPosition;
+              });
+            },
+            onPanUpdate: (details) {
+              if (!_isLeftMouseDragging) return;
+
+              if (_isSpacePressed) {
+                if (!widget.canPan) return;
+                final Offset delta =
+                    details.globalPosition - _gestureStartPosition;
+                final panMatrix = Matrix4.identity()
+                  ..translate(delta.dx, delta.dy);
+                widget.controller.transform = panMatrix * _gestureStartMat;
+              } else {
+                if (!widget.canZoom) return;
+                final double dy =
+                    details.globalPosition.dy - _gestureStartPosition.dy;
+                double scaleFactor = 1 + dy * _dragZoomSensitivity;
+                scaleFactor = max(0.01, scaleFactor);
+
+                final double startScale = _gestureStartMat.getMaxScaleOnAxis();
+                final double targetScale = startScale * scaleFactor;
+
+                if (targetScale < CanvasController.minScale) {
+                  scaleFactor = CanvasController.minScale / startScale;
+                } else if (targetScale > CanvasController.maxScale) {
+                  scaleFactor = CanvasController.maxScale / startScale;
+                }
+
+                final zoomMat = Matrix4.identity()
+                  ..translate(
+                    _gestureStartLocalPosition.dx,
+                    _gestureStartLocalPosition.dy,
+                  )
+                  ..scale(scaleFactor)
+                  ..translate(
+                    -_gestureStartLocalPosition.dx,
+                    -_gestureStartLocalPosition.dy,
+                  );
+                widget.controller.transform = zoomMat * _gestureStartMat;
+              }
+            },
+            onPanEnd: (details) {
+              if (!_isLeftMouseDragging) return;
+
+              final bool wasPanning = _isSpacePressed;
+              setState(() {
+                _isLeftMouseDragging = false;
+              });
+
+              final Offset pixelsPerSecond = details.velocity.pixelsPerSecond;
+              const double flingThreshold = 200.0;
+              if (pixelsPerSecond.distance < flingThreshold) return;
+
+              if (!wasPanning && widget.canZoom) {
+                widget.controller.flingZoom(
+                  pixelsPerSecond.dy,
+                  _gestureStartLocalPosition,
+                );
+              }
+            },
+            onPanCancel: () {
+              if (_isLeftMouseDragging) {
+                setState(() => _isLeftMouseDragging = false);
+              }
+            },
+            child: Container(
+              color: widget.backgroundColor,
+              child: ClipRect(
+                child: CanvasLayoutWidget(
+                  controller: widget.controller,
+                  gridType: widget.gridType,
+                  foregroundPainter: widget.foregroundPainter,
+                  children: widget.children,
+                ),
+              ),
             ),
           ),
         ),
--- lib/canvas/canvas_controller.dart
+++ lib/canvas/canvas_controller.dart
@@ -1,22 +1,25 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class CanvasController extends ChangeNotifier {
   static const double minScale = 0.1;
-  static const double maxScale = 4.0;
+  static const double maxScale = 50.0;
 
   Matrix4 _transform = Matrix4.identity();
   AnimationController? _animationController;
   Animation<Matrix4>? _animation;
-  final TickerProvider? vsync;
+  TickerProvider? _vsync;
 
-  CanvasController({this.vsync});
+  CanvasController();
+
+  void setVsync(TickerProvider vsync) {
+    _vsync = vsync;
+  }
 
   Matrix4 get transform => _transform;
 
   set transform(Matrix4 newTransform) {
-    if (_animationController?.isAnimating ?? false) {
-      _animationController!.stop();
-    }
+    stopAnimation();
     _setTransform(newTransform);
   }
 
@@ -32,7 +35,14 @@ class CanvasController extends ChangeNotifier {
     super.dispose();
   }
 
+  void stopAnimation() {
+    if (_animationController?.isAnimating ?? false) {
+      _animationController!.stop();
+    }
+  }
+
   void pan(Offset delta) {
+    if (delta == Offset.zero) return;
     final translationMatrix = Matrix4.identity()..translate(delta.dx, delta.dy);
     transform = translationMatrix * _transform;
   }
@@ -57,31 +67,64 @@ class CanvasController extends ChangeNotifier {
     transform = zoomMatrix * _transform;
   }
 
+  void flingZoom(double velocity, Offset focalPoint) {
+    if (_vsync == null || velocity.abs() < 1.0) return;
+
+    const double flingZoomSensitivity = 0.0005;
+    double targetScaleFactor = 1.0 + velocity * flingZoomSensitivity;
+    targetScaleFactor = max(0.01, targetScaleFactor);
+
+    final double currentScale = _transform.getMaxScaleOnAxis();
+    final double targetScale = currentScale * targetScaleFactor;
+
+    if (targetScale < minScale) {
+      targetScaleFactor = minScale / currentScale;
+    } else if (targetScale > maxScale) {
+      targetScaleFactor = maxScale / currentScale;
+    }
+
+    final targetZoomMatrix = Matrix4.identity()
+      ..translate(focalPoint.dx, focalPoint.dy)
+      ..scale(targetScaleFactor, targetScaleFactor)
+      ..translate(-focalPoint.dx, -focalPoint.dy);
+
+    final Matrix4 targetMatrix = targetZoomMatrix * _transform;
+
+    animateTo(
+      targetMatrix,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.decelerate,
+    );
+  }
+
   void animateTo(
     Matrix4 target, {
     Duration duration = const Duration(milliseconds: 350),
     Curve curve = Curves.easeInOut,
   }) {
-    if (vsync case final vsync?) {
-      _animationController?.stop();
-      _animationController ??= AnimationController(vsync: vsync)
-        ..addListener(_onAnimate);
-      _animationController!.duration = duration;
-
-      _animation = Matrix4Tween(
-        begin: _transform,
-        end: target,
-      ).animate(CurvedAnimation(parent: _animationController!, curve: curve));
-
-      _animationController!
-        ..reset()
-        ..forward();
+    if (_vsync == null) {
+      transform = target;
+      return;
     }
+
+    _animationController?.stop();
+    _animationController ??= AnimationController(vsync: _vsync!)
+      ..addListener(_onAnimate);
+    _animationController!.duration = duration;
+
+    _animation = Matrix4Tween(
+      begin: _transform,
+      end: target,
+    ).animate(CurvedAnimation(parent: _animationController!, curve: curve));
+
+    _animationController!
+      ..reset()
+      ..forward();
   }
 
   void _onAnimate() {
-    if (_animation case final animation?) {
-      _setTransform(animation.value);
+    if (_animation != null) {
+      _setTransform(_animation!.value);
     }
   }
 }
